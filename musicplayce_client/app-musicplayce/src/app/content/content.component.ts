7ac09d8c226b3386cc81a7309180a76cad213ca4
Fellipe Adorno <fellipeac2@gmail.com>
2019-10-09 15:12:00 -0300

    Delete function on front-end added

--- musicplayce_client/app-musicplayce/src/app/content/content.component.ts
+++ musicplayce_client/app-musicplayce/src/app/content/content.component.ts
@@ -30,12 +30,24 @@ export class ContentComponent implements OnInit {
 	}
 
 	onSave(value: string){
+		let aux = this;
 		if(this.stringMPC != undefined) {
 			console.log("Uploading value");
 			this.stringMPC.value =value;
 			this.myString.updateString(this.stringMPC)
 				.subscribe(
 					(res: StringMPC) => {
+						aux.stringMPC = res;
+					},
+					(error) => console.log(error)
+				);
+		} else {
+			console.log("Insert value");
+			this.myString.addString(value)
+				.subscribe(
+					(res: StringMPC) => {
+						console.log(res);
+						aux.stringMPC = res;
 					},
 					(error) => console.log(error)
 				);
@@ -43,6 +55,16 @@ export class ContentComponent implements OnInit {
 	}
 
 	onDelete() {
+		let aux = this;
+		this.stringMPC.value = "";
+		this.myString.deleteString(this.stringMPC._id)
+			.subscribe(
+				(res: StringMPC) => {
+					console.log("deleted");
+					aux.stringMPC = undefined;
+				},
+				(error) => console.log(error)
+			);
 	}
 
 }
--- musicplayce_client/app-musicplayce/src/app/string.service.ts
+++ musicplayce_client/app-musicplayce/src/app/string.service.ts
@@ -7,7 +7,7 @@ import { StringMPC } from './string.interface';
 @Injectable()
 export class StringService {
 
-	base_url : String = "http://localhost:8080/string";
+	base_url : String = "http://34.69.217.179:8080/string";
 
 	constructor(private http: HttpClient) { } 
 
@@ -15,8 +15,8 @@ export class StringService {
 		return this.http.get<StringMPC[]>(`${this.base_url}/`);
 	}
 
-	addString(newString:StringMPC) : Observable<StringMPC>{
-		return this.http.post<StringMPC>(`${this.base_url}`, newString, {
+	addString(newString:String ) : Observable<StringMPC>{
+		return this.http.post<StringMPC>(`${this.base_url}`, {"text": newString}, {
 			'headers': {'Content-Type': 'application/json'}
 		});
 	}
@@ -25,9 +25,14 @@ export class StringService {
 		console.log(`${this.base_url}/${editedStringMPC._id}`);
 		console.log(`${editedStringMPC.value}`);
 		return this.http.put<StringMPC>(`${this.base_url}/${editedStringMPC._id}`, {'text': editedStringMPC.value}, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
 	}
+
+	deleteString(id: String): Observable<StringMPC> {
+		return this.http.delete<StringMPC>(`${this.base_url}/${id}`);
+	}
+
 }
